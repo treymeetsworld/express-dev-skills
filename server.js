a7dc0a39ddb6193e2a46cbc5095612b7de1840ec
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-29 00:48:02 -0400

    modules setup

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
 
 // mounted routers
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
